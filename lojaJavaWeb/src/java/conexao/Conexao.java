3fd06814a2c2f6865463774fe7289a0b8ed4fcd6
brn043 <brunoescde@gmail.com>
2024-04-06 23:40:40 -0300

    começo back-end

---
+++ lojaJavaWeb/src/java/conexao/Conexao.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Conexao {
+    private static final String url = "jdbc:mysql://localhost:3306/database";
+    private static final String user = "root";
+    private static final String password = "root";
+    
+    public static Connection conectar() throws SQLException{
+        return (Connection) DriverManager.getConnection(url, user, password);
+    }    
+}
